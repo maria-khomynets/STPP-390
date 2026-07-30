6af86de0a90e38173b4a5057a89aacadffba1465
Igor Bilovash <belovash2002@gmail.com>
2026-07-30 18:36:39 +0300

    wip: work on reviews section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,1 +1,34 @@
-// Swiper initialization
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+function initReviewsSwiper() {
+  const reviewsSlider = document.querySelector('[data-reviews-swiper]');
+  if (!reviewsSlider) return;
+
+  new Swiper(reviewsSlider, {
+    modules: [Navigation],
+    watchSlidesProgress: true,
+    slidesPerView: 1,      
+    spaceBetween: 34,
+    grabCursor: true,
+    loop: true,
+
+    navigation: {
+      nextEl: '[data-swiper-next]',
+      prevEl: '[data-swiper-prev]',
+    },
+
+    breakpoints: {
+      1440: {
+        slidesPerView: 3,
+        spaceBetween: 57,
+      },
+    },
+  });
+}
+
+initReviewsSwiper();
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 // Main entry point for the application
+import './js/swiper.js';
