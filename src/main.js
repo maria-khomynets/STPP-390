3df4df1d5464f339a407e18e99d3d6068697e630
Maria Khomynets <masha.khomynets@gmail.com>
2026-08-01 12:30:56 +0300

    Add AOS scroll animations to sections

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 // Main entry point for the application
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './js/burgerMenu.js';
 import './js/headerScroll.js';
 import './js/swiper.js';
+
+AOS.init({
+  duration: 600,
+  easing: 'ease-out-cubic',
+  once: true,
+  offset: 80,
+  disable: 'prefers-reduced-motion',
+});
